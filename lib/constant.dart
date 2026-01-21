aac676b4790231dd40850e191cca4686b01225b9
Jatin Sharma <80152469+sharmajatin1997@users.noreply.github.com>
2026-01-21 13:27:17 +0530

    Update constant.dart

--- lib/constant.dart
+++ lib/constant.dart
@@ -1,8 +1,8 @@
 class Constant {
 
    //==== Agora Details ====
-   static const String appId = '2c24f7d317c44b8ab58ba4d16d2ee9f4';
-   static const String channelName = 'AstroVoice';
-   static const String rtcToken ='007eJxTYGA9wDYtfWFirIh/vMvhpJV58ZkJV4SXrp7CdtWt6EtUZqwCg1GykUmaeYqxoXmyiUmSRWKSqUVSokmKoVmKUWqqZZpJdk5BZkMgI8M35qusjAwQCOJzMTgWlxTlh+VnJqcyMAAAibUgmQ==';
+   static const String appId = '';
+   static const String channelName = '';
+   static const String rtcToken ='';
 
 }
